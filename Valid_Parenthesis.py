c0392c94f959c1237be33035ec0bd4152eb7d83c
DharaniPulkaram7 <pulkaramdharani1@gmail.com>
2025-07-08 10:59:23 +0530

    Add files via upload

---
+++ Valid_Parenthesis.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        paran={"}":"{","]":"[",")":"("}
+        stack=[]
+        for char in s:
+            if char in paran:
+                top=stack.pop() if stack else '#'
+                if paran[char]!=top:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
